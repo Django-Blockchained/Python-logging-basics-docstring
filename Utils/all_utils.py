26afd050d45ca39fc5fda935f88b5a381da90dc6
Vishal Pandey <vishalp7711@gmal.com>
2022-10-04 17:58:45 +0530

    Logging for OD added

--- Utils/all_utils.py
+++ Utils/all_utils.py
@@ -2,9 +2,20 @@ import os
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
+import logging
 
 
 def prepare_data(df, target_col="y"):
+    """it returns label and independent features
+
+    Args:
+        df (pd.DataFrame): This is a dataframe
+        target_col (str, optional): label col name. Defaults to "y".
+
+    Returns:
+        tuple: label and x
+    """
+    logging.info("Preparing the data for training")
     X = df.drop(target_col, axis=1)
     
     y = df[target_col]
@@ -13,6 +24,7 @@ def prepare_data(df, target_col="y"):
 
 def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     def _create_base_plot(df):
+        logging.info("creating the base plot")
         df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="coolwarm")
         plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
         plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
@@ -21,6 +33,7 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
         figure.set_size_inches(10, 8)
     
     def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        logging.info("plotting the decision regions")
         colors = ("cyan", "lightgreen")
         cmap = ListedColormap(colors)
         
@@ -51,3 +64,4 @@ def save_plot(df, model, filename="plot.png", plot_dir="plots"):
     os.makedirs(plot_dir, exist_ok=True)
     plot_path = os.path.join(plot_dir, filename)
     plt.savefig(plot_path)
+    logging.info(f"saving the plot at {plot_path}")
